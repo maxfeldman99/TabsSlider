600d245b72c4eaf0f72c0d01d18abee3def9794e
MAX FELDMAN <MAX FELDMAN@DESKTOP-56O7A44>
2018-04-04 00:20:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/maxfeldman/tabsslider/ThreeFragment.java
@@ -0,0 +1,31 @@
+package com.example.maxfeldman.tabsslider;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by MAX FELDMAN on 03/04/2018.
+ */
+
+public class ThreeFragment extends Fragment {
+
+    public ThreeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+
+}
